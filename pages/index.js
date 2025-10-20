47e9b63f311185a6235f41308e2723c70b4c6132
Brunovenson <brunovenson@icloud.com>
2025-10-20 18:32:33 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
 function Home() {
-    return <h1>Teste</h1>
-}
+    return <h1>O sucesso é a soma de pequenos esforços repetidos dia e noite e A persistência é o caminho do êxito</h1>
+}"
 export default Home;
