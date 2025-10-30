492610f29640c4f41a5d91d4ac0788a3794a0eae
Brunovenson <brunovenson@icloud.com>
2025-10-30 21:41:19 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>"O sucesso é a soma de pequenos esforços repetidos dia e noite" e "A persistência é o caminho do êxito"😉</h1>
+  return (
+    <h1>
+      "O sucesso é a soma de pequenos esforços repetidos dia e noite" e "A
+      persistência é o caminho do êxito"😉
+    </h1>
+  );
 }
 
 export default Home;
